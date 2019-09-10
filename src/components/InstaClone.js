47e5aa221d1529fe642d3702b98404bcffab4291
Joseph Pulaski <jpulaski79@gmail.com>
2019-09-10 08:28:47 -0400

    updates to InstaClone

--- src/components/InstaClone.js
+++ src/components/InstaClone.js
@@ -4,14 +4,14 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 export class InstaClone extends Component {
     render() {
         return (
-            <View>
-                <Text> Instagram Clone Main Component</Text>
-                <Image 
-                    style={styles.stretch}
+            <View style={styles.container}>
+                <View style={styles.tempNav}/>
+                <Image style={styles.stretch}
                     source= {{ 
                         uri:  "https://lh3.googleusercontent.com/RK1yxVSm8gQmpP8uZ1OxMqtWw4V1S_REoXhzAqOtgPF9N6eSmHr-VPWniq_8TZ1yGdjk22Y5H9-DXbqO8l_LZVSlqyw"
                     }}
                 />
+
             </View>
         )
     }
@@ -19,12 +19,23 @@ export class InstaClone extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        width: "100%",
+        height: "100%",
     },
-
+    tempNav: {
+        width: "100%",
+        height: 75,
+        backgroundColor: "rgb(250,250,150)",
+        borderBottomColor: "rgb(233,33,233)",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },  
+    text: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
     stretch: {
-      width: 200,
-      height: 200,
-      resizeMode: 'stretch'
+      width: "100%",
+      height: 100,
     }
   });
