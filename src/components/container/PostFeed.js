c56af5ebefd168a1a97224ee8d089fbbfa04a48e
Joseph Pulaski <jpulaski79@gmail.com>
2019-09-12 08:56:24 -0400

    PostFeed NOT rendering Posts

--- src/components/container/PostFeed.js
+++ src/components/container/PostFeed.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 import Post from '../presentation/Post'
 
 export default class PostFeed extends Component {
+
+    _renderPost() {
+        return ({ item }) =>  <Post />;
+    }
+
+    _returnKey(item) {
+        return ( item );
+    }
     render() {
         return (
-            <View>
-                <Post />
-            </View>
-        )
+            <FlatList
+                data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+                keyExtractor={this._returnKey}
+                renderItem={() => this._renderPost}
+            />
+
+        );
     }
 }
-
-const styles = StyleSheet.create({})
